42de896e8036e7d4d2e34f695f9f655f6163b1ee
Felix Ernst <felix.gm.ernst@outlook.com>
2020-11-24 17:28:56 +0100

    cleaned up prevalence calculation and added taxonomic rank selection

--- R/getPrevalence.R
+++ R/getPrevalence.R
@@ -1,60 +1,101 @@
-#' @title Taxa Prevalence
-#' 
-#' @name getPrevalence
-#' 
-#' @description This function estimates population prevalence for microbial taxa in a \code{\link{SummarizedExperiment-class}} object.
-#' 
-#' @param x \code{\link{SummarizedExperiment-class}} object
-#' 
-#' @param detection Detection threshold for absence/presence
-#' (strictly greater by default; in 0 to 1).
-#' 
-#' @param include_lowest Include the lower boundary of the detection and
-#' prevalence cutoffs. FALSE by default.
-#' 
-#' @param sort Sort the groups by prevalence (default: FALSE)
-#' 
-#' @param as_relative Logical. Apply detection threshold on compositional (relative) abundances.
-#' 
+#' Prevalence calculation
+#'
+#' These functions calculate the population prevalence for taxonomic ranks in a
+#' \code{\link{SummarizedExperiment-class}} object.
+#'
+#' @param x a
+#'   \code{\link[SummarizedExperiment:SummarizedExperiment-class]{SummarizedExperiment}}
+#'   object
+#'
+#' @param detection Detection threshold for absence/presence. Either an
+#'   absolutes value compared directly to the values of \code{x} or a relative
+#'   value between 0 and 1, if \code{as_relative = TRUE}.
+#'
+#' @param include_lowest logical scalar: Should the lower boundary of the
+#'   detection and prevalence cutoffs be included? (default: \code{FALSE})
+#'
+#' @param sort logical scalar: Should the result be sorted by prevalence?
+#'   (default: \code{FALSE})
+#'
+#' @param as_relative logical scalar: Should the detection threshold be applied
+#'   on compositional (relative) abundances? (default: \code{TRUE})
+#'
 #' @param abund_values A single character value for selecting the
 #'   \code{\link[SummarizedExperiment:SummarizedExperiment-class]{assay}}
-#'   to use for calculating the relative abundance.
-#' 
-#' @param ... Arguments to pass.
-#' 
-#' @details Calculates the frequency (between 0 and 1) of samples that exceed the
-#' detection threshold. For SummarizedExperiment objects, calculates this for each
-#' taxa. The absolute population prevalence can be obtained by multiplying the
-#' prevalence by the number of samples.
-#' 
-#' @return For each taxa, the frequency of samples where a given taxa is
-#' detected at a given detection threshold. The output is provided as a
-#' frequency (between 0 and 1).
-#' 
-#' @references 
-#' A Salonen et al. The adult intestinal core microbiota is determined by 
-#' analysis depth and health status. Clinical Microbiology and Infection 
-#' 18(S4):16 20, 2012. 
-#' To cite the R package, see citation('mia')
-#' 
-#' @author Leo Lahti
-#' 
-#' @keywords utilities
-#' 
+#'   to use for prevalence calculation.
+#'
+#' @param rank,... additional arguments
+#' \itemize{
+#'   \item{If \code{!is.null(rank)} arguments are passed on to
+#'   \code{\link[=agglomerate-methods]{agglomerateByRank}}. See
+#'   \code{\link[=agglomerate-methods]{?agglomerateByRank}} for more details.
+#'   }
+#' }
+#'
+#' @details
+#' \code{getPrevalence} calculates the relative frequency of samples that exceed
+#' the detection threshold. For \code{SummarizedExperiment} objects, the
+#' prevalence is calculated for the selected taxonomic rank, otherwise for the
+#' rows. The absolute population prevalence can be obtained by multiplying the
+#' prevalence by the number of samples (\code{ncol(x)}). If \code{as_relative =
+#' TRUE} the relative frequency (between 0 and 1) is used to check against the
+#' \code{detection} threshold.
+#'
+#' @return
+#' a named \code{numeric} vector. The names are either the row names of \code{x}
+#' or the names after agglomeration. For \code{getPrevalentTaxa} only the names
+#' exceeding the threshold set by \code{prevalence} are returned.
+#'
+#' @seealso
+#' \code{\link[=agglomerate-methods]{agglomerateByRank}}
+#'
+#'
+#' @name getPrevalence
 #' @export
-#' 
+#'
+#' @references
+#' A Salonen et al. The adult intestinal core microbiota is determined by
+#' analysis depth and health status. Clinical Microbiology and Infection
+#' 18(S4):16 20, 2012.
+#' To cite the R package, see citation('mia')
+#'
+#' @author Leo Lahti
+#'
+#' @export
+#'
 #' @examples
+#' data(GlobalPatterns)
+#' # Get prevalence estimates for individual ASV/OTU
+#' prevalence.frequency <- getPrevalence(GlobalPatterns,
+#'                                       detection = 0,
+#'                                       sort = TRUE,
+#'                                       as_relative = TRUE)
+#' head(prevalence.frequency)
 #'
-#' # Get prevalence estimates for microbial taxa (population frequencies)
-#' data(GlobalPatterns)     
-#' prevalence.frequency <- getPrevalence(GlobalPatterns, detection=0, sort=TRUE, as_relative=TRUE)
-#' print(head(prevalence.frequency))
-#'
-#' # Get prevalence estimates for microbial taxa (population counts)
+#' # Get prevalence estimates for phylums
 #' # - the getPrevalence function itself always returns population frequencies
-#' # - to obtain population counts, multiply frequencies with the sample size:
+#' # - to obtain population counts, multiply frequencies with the sample size,
+#' #   which answers the question "In how many samples is this phylum detectable"
+#' prevalence.frequency <- getPrevalence(GlobalPatterns,
+#'                                       rank = "Phylum",
+#'                                       detection = 0,
+#'                                       sort = TRUE,
+#'                                       as_relative = TRUE)
+#' head(prevalence.frequency)
 #' prevalence.count <- prevalence.frequency * ncol(GlobalPatterns)
-
+#' head(prevalence.count)
+#'
+#' # Detection threshold 1 (strictly greater by default);
+#' # Note that the data (GlobalPatterns) is here in absolute counts
+#' # (and not compositional, relative abundances)
+#' # Prevalence threshold 50 percent (strictly greater by default)
+#' taxa <- getPrevalentTaxa(GlobalPatterns,
+#'                          rank = "Phylum",
+#'                          detection = 1/100,
+#'                          prevalence = 50/100,
+#'                          as_relative = TRUE)
+#' head(taxa)
+NULL
 
 #' @rdname getPrevalence
 #' @export
@@ -65,58 +106,97 @@ setGeneric("getPrevalence", signature = "x",
 #' @rdname getPrevalence
 #' @export
 setMethod("getPrevalence", signature = c(x = "ANY"),
-    function(x, detection, include_lowest, sort, as_relative, ...){
-
-        if (!is.numeric(detection)) {
-            stop("The detection argument in getPrevalence function should be numeric.")
-        } 
-    
-        if (include_lowest) {
-            prev <- rowSums(x >= detection)
-        } else {
-            prev <- rowSums(x > detection)
+    function(x, detection = 0, include_lowest = FALSE, sort = FALSE, ...){
+        # input check
+        if (!.is_numeric_string(detection)) {
+            stop("'detection' must be ainslge numeric value or coercibel to ",
+                 "one.",
+                 call. = FALSE)
         }
-
+        detection <- as.numeric(detection)
+        if(!.is_a_bool(include_lowest)){
+            stop("'include_lowest' must be TRUE or FALSE.", call. = FALSE)
+        }
+        if(!.is_a_bool(sort)){
+            stop("'sort' must be TRUE or FALSE.", call. = FALSE)
+        }
+        #
+        if (include_lowest) {
+            prev <- x >= detection
+        } else {
+            prev <- x > detection
+        }
+        prev <- rowSums(prev)
         # Always return prevalence as a relative frequency.
-        # This helps to avoid confusion with detection limit, which
-        # is applied on either relative abundances or absolute counts
-        # with as_relative argument
-        prev <- prev/ncol(x)
-    
+        # This helps to avoid confusion with detection limit
+        prev <- prev / ncol(x)
         if (sort) {
             prev <- rev(sort(prev))
         }
-    
-        # Return
         prev
-
     }
 )
 
 #' @rdname getPrevalence
 #' @export
 setMethod("getPrevalence", signature = c(x = "SummarizedExperiment"),
-    function(x, detection=0, include_lowest=FALSE, sort=FALSE, as_relative=TRUE, abund_values = "counts", ...){
-
-        if (as_relative) { 
-            # Add relative abundances if not yet available
-            x <- relAbundanceCounts(x)
-            abund_values <- "relabundance"
-        } 
-
+    function(x, abund_values = "counts", as_relative = TRUE,
+             rank = NULL, ...){
+        if(!.is_a_bool(as_relative)){
+            stop("'as_relative' must be TRUE or FALSE.", call. = FALSE)
+        }
         # check assay
         .check_abund_values(abund_values, x)
-
+        if(!is.null(rank)){
+            x <- agglomerateByRank(x, rank = rank, ...)
+        }
+        mat <- assay(x, abund_values)
+        if (as_relative) {
+            mat <- .calc_rel_abund(mat)
+        }
         # retrieve abundance matrix
-        x <- assay(x, abund_values)
-    
-        getPrevalence(x, detection=detection,
-            include_lowest=include_lowest,
-            sort=sort,
-            as_relative=as_relative, ...)
-    
+        getPrevalence(mat, ...)
     }
 )
 
+#' @rdname getPrevalence
+#'
+#' @param prevalence Prevalence threshold (in 0 to 1). The
+#' required prevalence is strictly greater by default. To include the
+#' limit, set include_lowest to TRUE.
+#'
+#' @details
+#' \code{getPrevalentTaxa} returns taxa that are more prevalent with the
+#' given detection threshold for the selected taxonomic rank.
+#'
+#' @export
+setGeneric("getPrevalentTaxa", signature = "x",
+           function(x, ...)
+               standardGeneric("getPrevalentTaxa"))
 
-
+#' @rdname getPrevalence
+#' @export
+setMethod("getPrevalentTaxa", signature = c(x = "SummarizedExperiment"),
+    function(x, prevalence = 50/100, rank = taxonomyRanks(x)[1L],
+             include_lowest = FALSE, ...){
+        # input check
+        if (!.is_numeric_string(prevalence)) {
+            stop("'prevalence' must be a single numeric value or coercibel to ",
+                 "one.",
+                 call. = FALSE)
+        }
+        prevalence <- as.numeric(prevalence)
+        if(!.is_a_bool(include_lowest)){
+            stop("'include_lowest' must be TRUE or FALSE.", call. = FALSE)
+        }
+        #
+        pr <- getPrevalence(x, rank = rank, ...)
+        if (include_lowest) {
+          taxa <- pr >= prevalence
+        } else {
+          taxa <- pr > prevalence
+        }
+        taxa <- names(which(taxa))
+        taxa
+    }
+)
--- tests/testthat/test-5prevalence.R
+++ tests/testthat/test-5prevalence.R
@@ -3,25 +3,34 @@ context("prevalence")
 test_that("getPrevalence", {
 
     data(GlobalPatterns)
-    
-    # Output should be always a frequency between 0 to 1	
+    expect_error(getPrevalence(GlobalPatterns, detection="test"),
+                 "'detection' must be ainslge numeric value or coercibel to one")
+    expect_error(getPrevalence(GlobalPatterns, include_lowest="test"),
+                 "'include_lowest' must be TRUE or FALSE")
+    expect_error(getPrevalence(GlobalPatterns, sort="test"),
+                 "'sort' must be TRUE or FALSE")
+    expect_error(getPrevalence(GlobalPatterns, as_relative="test"),
+                 "'as_relative' must be TRUE or FALSE")
+    expect_error(getPrevalence(GlobalPatterns, abund_values="test"),
+                 "'abund_values' must be a valid name")
+    # Output should be always a frequency between 0 to 1
     pr <- getPrevalence(GlobalPatterns, detection=0.1/100, as_relative=TRUE)
     expect_true(min(pr) >= 0 && max(pr) <= 1)
     pr <- getPrevalence(GlobalPatterns, detection=0.1/100, as_relative=FALSE)
-    expect_true(min(pr) >= 0 && max(pr) <= 1)    
+    expect_true(min(pr) >= 0 && max(pr) <= 1)
 
     # Same prevalences should be returned for as_relative T/F in certain cases.
     pr1 <- getPrevalence(GlobalPatterns, detection=1, include_lowest=TRUE, as_relative=FALSE)
-    pr2 <- getPrevalence(GlobalPatterns, detection=0/100, include_lowest=FALSE, as_relative=TRUE)        
+    pr2 <- getPrevalence(GlobalPatterns, detection=0/100, include_lowest=FALSE, as_relative=TRUE)
     expect_true(all(pr1 == pr2))
 
     # Same prevalences should be returned for as_relative T/F in certain cases.
     pr1 <- getPrevalence(GlobalPatterns, detection=1, include_lowest=TRUE, as_relative=FALSE)
-    pr2 <- getPrevalence(GlobalPatterns, detection=0, include_lowest=FALSE, as_relative=FALSE)        
+    pr2 <- getPrevalence(GlobalPatterns, detection=0, include_lowest=FALSE, as_relative=FALSE)
     expect_true(all(pr1 == pr2))
 
     # Different ways to use relative abundance should yield the same output
-    pr2 <- getPrevalence(GlobalPatterns, as_relative=TRUE, abund_values = "counts")            
+    pr2 <- getPrevalence(GlobalPatterns, as_relative=TRUE, abund_values = "counts")
     GlobalPatterns <- relAbundanceCounts(GlobalPatterns)
     pr1 <- getPrevalence(GlobalPatterns, as_relative=FALSE, abund_values = "relabundance")
     expect_true(all(pr1 == pr2))
@@ -36,25 +45,26 @@ test_that("getPrevalence", {
 test_that("getPrevalentTaxa", {
 
     data(GlobalPatterns)
-    
+    expect_error(getPrevalentTaxa(GlobalPatterns, prevalence="test"),
+                 "'prevalence' must be a single numeric value or coercibel to one")
     # Results compatible with getPrevalence
     pr1 <- getPrevalentTaxa(GlobalPatterns, detection=0.1/100, as_relative=TRUE, sort=TRUE)
-    pr2 <- names(getPrevalence(GlobalPatterns, detection=0.1/100, as_relative=TRUE, sort=TRUE))
+    pr2 <- names(getPrevalence(GlobalPatterns, rank = "Kingdom", detection=0.1/100, as_relative=TRUE, sort=TRUE))
     expect_true(all(pr1 == pr2))
 
     # Same sorting for toptaxa obtained in different ways
-    pr1 <- sort(getPrevalentTaxa(GlobalPatterns, detection=0.1/100, as_relative=TRUE, sort=FALSE))
-    pr2 <- names(getPrevalence(GlobalPatterns, detection=0.1/100, as_relative=TRUE, sort=TRUE))
+    pr1 <- getPrevalentTaxa(GlobalPatterns, detection=0.1/100, as_relative=TRUE, sort=TRUE)
+    pr2 <- names(getPrevalence(GlobalPatterns, rank = "Kingdom", detection=0.1/100, as_relative=TRUE, sort=TRUE))
     expect_true(all(pr1 == pr2))
 
     # Retrieved taxa are the same for counts and relative abundances
     pr1 <- getPrevalentTaxa(GlobalPatterns, detection=0.1/100, as_relative=TRUE)
-    pr2 <- getPrevalentTaxa(GlobalPatterns, detection=0.1/100, as_relative=FALSE)    
+    pr2 <- getPrevalentTaxa(GlobalPatterns, detection=0.1/100, as_relative=FALSE)
     expect_true(all(pr1 == pr2))
 
     # Prevalence and detection threshold at 0 has the same impact on counts and relative abundances
     pr1 <- getPrevalentTaxa(GlobalPatterns, detection=0, prevalence=0, as_relative=TRUE)
     pr2 <- getPrevalentTaxa(GlobalPatterns, detection=0, prevalence=0, as_relative=FALSE)
     expect_true(all(pr1 == pr2))
-    
+
 })
