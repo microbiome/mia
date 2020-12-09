b3c2ba195f32a68cf80cd09f7cb1c4cd5685a7c5
Felix Ernst <felix.gm.ernst@outlook.com>
2020-12-09 15:08:20 +0100

    added test for DMN functions

---
+++ tests/testthat/test-7dmn.R
@@ -0,0 +1,20 @@
+context("DMN")
+test_that("DMN", {
+    data(dmn_se)
+    actual <- calculateDMN(dmn_se)
+    expect_true(is.list(actual))
+    expect_s4_class(actual[[1L]],"DMN")
+    # return a list of DMN objects
+    actual <- getDMN(dmn_se)
+    expect_true(is.list(actual))
+    expect_s4_class(actual[[1L]],"DMN")
+    # return, which objects fits best
+    expect_equal(bestDMNFit(dmn_se, type = "laplace"), 4)
+    # return the model, which fits best
+    actual <- getBestDMNFit(dmn_se, type = "laplace")
+    expect_true(is.list(actual))
+    expect_s4_class(actual[[1L]],"DMN")
+    # plot the fit
+    actual <- plotDMNFit(dmn_se, type = "laplace")
+    expect_s3_class(actual, c("gg","ggplot"))
+})
