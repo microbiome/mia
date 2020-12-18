66cfd624765976b69959a93dda93f02ed9a5b8ec
Tuomas Borman <tvborm@utu.fi>
2020-12-18 18:49:46 +0200

    Added unit tests for estimateDominance

--- tests/testthat/test-5dominance.R
+++ tests/testthat/test-5dominance.R
@@ -1,5 +1,29 @@
-context("dominance")
+context("estimateDominance")
 
 test_that("estimateDominance", {
+
+    data(esophagus)
+
+    #Calculates index that is not accepted, should get an error.
+    expect_error((estimateDominance(esophagus, index = "shannon")))
+
+    #Calculates DBP in two different ways, should get the same result.
+    expect_true(all(estimateDominance(esophagus)$DBP == estimateDominance(esophagus, index = "DBP")@colData$DBP))
+
+    #Calculates absolute in two different ways, should get the same result.
+    expect_true(all(estimateDominance(esophagus, index = NULL, relative = FALSE, rank = 1)@colData$absolute ==
+                        estimateDominance(esophagus, index = "absolute")@colData$absolute))
+
+    #Calculates relative in two different ways, should get the same result.
+    expect_true(all(estimateDominance(esophagus, index = NULL, relative = TRUE, rank = 1)@colData$relative ==
+                        estimateDominance(esophagus, index = "relative")@colData$relative))
+
+    #Calculates DBP in two different ways, should get the same result.
+    expect_true(all(estimateDominance(esophagus, index = NULL, relative = TRUE, rank = 1)@colData$DBP ==
+                        estimateDominance(esophagus, index = "DBP")@colData$DBP))
+
+    #Calculates DMN in two different ways, should get the same result.
+    expect_true(all(estimateDominance(esophagus, index = NULL, relative = TRUE, rank = 2)@colData$DMN ==
+                        estimateDominance(esophagus, index = "DMN")@colData$DMN))
 
 })
