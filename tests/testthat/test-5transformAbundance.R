2b0c723288fecb08b7a996e479adbcdc32c1b319
Tuomas Borman <tvborm@utu.fi>
2021-01-21 19:27:05 +0200

    Added some tests

--- tests/testthat/test-5transformAbundance.R
+++ tests/testthat/test-5transformAbundance.R
@@ -6,7 +6,31 @@ test_that("transformAbundance", {
     data(GlobalPatterns)
     tse <- GlobalPatterns
 
-    # Tests come here.......................
+    # No method specified. Should be an error.
+    testthat::expect_error(mia::transformAbundance(tse))
+
+    # Method is not provided. Should be an error.
+    testthat::expect_error(mia::transformAbundance(tse, method="test"))
+
+    # Counts table should not be changed
+    testthat::expect_equal(assays(mia::transformAbundance(tse, method = "pa"))$counts, assays(tse)$counts)
+
+    # Calculates relative abundances. Should be equal.
+    testthat::expect_equal(assays(mia::transformAbundance(tse, method = "relabundance"))$relabundance,
+                           apply(assays(tse)$counts, 2, FUN=function(x){
+                               x/sum(x)
+                           }))
+
+    # Calculates log10 transformation with pseudocount = 1. Should be equal.
+    testthat::expect_equal(assays(mia::transformAbundance(tse, method = "log10", pseudocount = 1))$log10,
+                           apply(assays(tse)$counts, 2, FUN=function(x){
+                               log10(x+1)
+                           }))
+
+    # Calculates pa transformation. Should be equal.
+    testthat::expect_equal(as.vector(assays(mia::transformAbundance(tse, method = "pa"))$pa),
+                           as.integer(assays(tse)$counts > 0))
+
 
 })
 
