331d089b624c34850e5695717d5e3ff443582186
tvborm <tvborm@utu.fi>
2021-05-27 11:30:29 +0300

    Minor change

--- R/estimateDiversity.R
+++ R/estimateDiversity.R
@@ -464,6 +464,7 @@ setMethod("estimateFaith", signature = c(x="TreeSummarizedExperiment", tree="mis
     log(1 + r)
 }
 
+#' @importFrom DelayedMatrixStats rowSums2 rowMeans2
 .calc_skewness <- function(x) {
     # Transposes the table
     x <- t(x)
