40bc47400c33c48d6fd1b1e52dab3a48dfe9eebe
Tuomas Borman <tvborm@utu.fi>
2021-08-03 09:52:23 +0300

    Revert changes related to adding ranks to agglomerated rownames

--- tests/testthat/test-5prevalence.R
+++ tests/testthat/test-5prevalence.R
@@ -151,10 +151,13 @@ test_that("getRareTaxa", {
 
         # Gets rownames for all the taxa
         all_taxa <- rownames(se)
-        
-        # Prevalent and rare taxa include only taxa from specified rank. all taxa
-        # might include taxa from higher rank.
-        all_taxa <- all_taxa[stringr::str_detect(all_taxa, rank)]
+
+        # All but "Kingdom" can includes different taxa levels and e.g. "Species"
+        # before their name
+        if( rank != "Kingdom" ){
+            # Takes e.g. only species and removes e.g. "Species:" from the names
+            all_taxa <- stringr::str_remove(all_taxa[grepl(paste0(rank, ":"), all_taxa)], paste0(rank, ":"))
+        }
 
         # Gets prevalent taxa
         prevalent_taxa <- getPrevalentTaxa(GlobalPatterns,
