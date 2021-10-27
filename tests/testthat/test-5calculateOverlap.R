1158a864d29b52a5f58be804abb63fd7886a1b93
Tuomas Borman <tvborm@utu.fi>
2021-10-27 21:53:18 +0300

    Fix

--- tests/testthat/test-5calculateOverlap.R
+++ tests/testthat/test-5calculateOverlap.R
@@ -23,7 +23,7 @@ test_that("calculateOverlap", {
     # reference <- microbiome::overlap(esophagus, detection = 0.15)
     reference <- matrix(c(0.0000000, 0.3799961, 0.4111838, 0.3799961, 0.0000000,
                           0.3634972, 0.4111838, 0.3634972, 0.0000000), nrow=3)
-    expect_equal(unname(result), reference)
+    expect_equal(round(unname(result), 7), round(reference), 7)
     # Test with different detection threshold
     result <- calculateOverlap(tse, abund_values = "relabundance", detection = 0)
     result <- as.matrix(result)
@@ -32,6 +32,6 @@ test_that("calculateOverlap", {
     # reference <- microbiome::overlap(esophagus, detection = 0)
     reference <- matrix(c(0.0000000, 0.8811552, 0.9038734, 0.8811552, 0.0000000, 
                           0.8390008, 0.9038734, 0.8390008, 0.00000000), nrow=3)
-    expect_equal(unname(result), reference)
+    expect_equal(round(unname(result), 7), round(reference, 7))
 })
 
