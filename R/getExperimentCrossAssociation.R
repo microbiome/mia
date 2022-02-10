3cbf21e73a97d4cf4550bf44fc6e57d4051da861
Tuomas Borman <tvborm@utu.fi>
2022-02-10 21:25:18 +0200

    Update

--- R/getExperimentCrossAssociation.R
+++ R/getExperimentCrossAssociation.R
@@ -317,7 +317,8 @@ setMethod("testExperimentCrossAssociation", signature = c(x = "ANY"),
     if( !.is_non_empty_string(direction) && !direction %in% c("row", "column") ){
       stop("'direction' must be 'row' or 'column'.", call. = FALSE)
     }
-    # Method is checked in .calculate_association. Otherwise association_FUN would
+    # Check method
+    # method is checked in .calculate_association. Otherwise association_FUN would
     # not work. (It can be "anything", and it might also have method parameter.)
     # Check mode
     mode <- match.arg(mode, c("table", "matrix"))
