02b02a63e10a86d1b0a28a47778deaf755bdbfae
Tuomas Borman <tvborm@utu.fi>
2022-06-29 19:32:12 +0300

    Update

--- R/mergeTreeSummarizedExperiment.R
+++ R/mergeTreeSummarizedExperiment.R
@@ -5,6 +5,10 @@
 #' 
 #' @param y a \code{\link{SummarizedExperiment}} object 
 #' 
+#' @param abund_values A single character value for selecting the
+#' \code{\link[SummarizedExperiment:SummarizedExperiment-class]{assay}}
+#' to be merged.
+#' 
 #' @param missing_values NA, 0, or a single character values specifying the notation
 #' of missing values.
 #'
@@ -45,13 +49,47 @@ NULL
 #' @rdname mergeTreeSummarizedExperiment
 #' @export
 setGeneric("mergeTreeSummarizedExperiment", signature = c("x"),
-        function(x, missing_values = 0, ..., BPPARAM = SerialParam() )
+        function(x, abund_values = "counts", missing_values = 0, ..., BPPARAM = SerialParam() )
             standardGeneric("mergeTreeSummarizedExperiment"))
 
 #' @rdname mergeTreeSummarizedExperiment
 #' @export
 setMethod("mergeTreeSummarizedExperiment", signature = c(x = "list"),
-        function(x, missing_values = 0, ..., BPPARAM = SerialParam() ){
+        function(x, abund_values = "counts", missing_values = 0, ..., BPPARAM = SerialParam() ){
+            ################## Input check ##################
+            # Can the abund_value tbe found form all the objects
+            abund_values_bool <- lapply(x, .assay_cannot_be_found, abund_values = abund_values)
+            if( any(abund_values_bool) ){
+                stop("'abund_values' must specify an assay from assays. 'abund_values' ",
+                     "cannot be found at least in one TreeSE.",
+                     call. = FALSE)
+            }
+            # Is missing_values one of the allowed ones
+            missing_values_bool <- (is.numeric(missing_values) && missing_values = 0) ||
+                .is_a_string(missing_values) || is.na(missing_values)
+            # If not then give error
+            if(  missing_values_bool ){
+                stop("'missing_values' must be 0, NA, or a single character value.",
+                     call. = FALSE)
+            }
+            ################ Input check end ################
+            # Take first element and remove it from the list
+            tse <- x[[1]]
+            x[[1]] <- NULL
+            # Remove all information but rowData, colData, and assay
+            rowdata <- rowData(tse)
+            colData(tse) <- colData(tse)
+            assay <- assay(tse, abund_values)
+            assays <- SimpleList(name = assay)
+            names(assays) <- abund_values
+            tse <- TreeSummarizedExperiment(assays = assays,
+                                            rowData = rowdata,
+                                            colData = colData
+                                            )
+            
+            for(i in list ){
+                tse <- .merge_TreeSE(i, tse_original = tse, missing_values = missing_values)
+            }
               
         }
 )
@@ -75,3 +113,38 @@ setMethod("mergeTreeSummarizedExperiment", signature = c(x = "SummarizedExperime
             mergeTreeSummarizedExperiment(list, ...)
         }
 )
+
+################################ HELP FUNCTIONS ################################
+
+.assay_cannot_be_found <- function(abund_values, obj){
+    tryCatch(
+        {
+            .check_assay_present(abund_values, obj)
+            return(FALSE)
+            
+        },
+        error=function(cond) {
+            
+            return(TRUE)
+        }
+    )
+}
+
+.merge_TreeSE <- function(tse_original, tse, missing_values){
+    rowdata <- .merge_rowdata(tse_original, tse)
+    coldata <- .merge_coldata(tse_original, tse)
+    assays <- .merge_assays(tse_original, tse)
+}
+
+.merge_rowdata <- function(tse_original, tse){
+    
+}
+
+.merge_coldata <- function(tse_original, tse){
+    
+}
+
+.merge_assays <- function(tse_original, tse){
+    
+}
+
