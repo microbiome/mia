f1773e064970cb94ea9bc5de055ea40df69db10a
Tuomas Borman <tvborm@utu.fi>
2022-08-15 13:27:58 +0300

    Add .is_an_integer to utils

--- R/utils.R
+++ R/utils.R
@@ -39,6 +39,10 @@
   abs(x - round(x)) <= tol && !is.infinite(x)
 }
 
+.is_an_integer <- function(x){
+    is.numeric(x) && length(x) == 1L && x%%1==0
+}
+
 .is_numeric_string <- function(x){
   x <- as.character(x)
   suppressWarnings({x <- as.numeric(x)})
