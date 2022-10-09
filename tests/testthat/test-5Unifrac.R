503e6342499aa6d10bf461a776ba87328388a815
Tuomas Borman <tvborm@utu.fi>
2022-10-09 21:13:56 +0300

    up

--- tests/testthat/test-5Unifrac.R
+++ tests/testthat/test-5Unifrac.R
@@ -51,4 +51,35 @@ test_that("Unifrac beta diversity", {
                          weighted = FALSE, normalized = 1,
                          BPPARAM = SerialParam())
     )
+    
+    data("GlobalPatterns")
+    tse <- GlobalPatterns
+    # Compare to phyloseq function if it is installed
+    if( !require("phyloseq") ){
+        # Convert data into phyloseq
+        pseq <- makePhyloseqFromTreeSE(tse)
+        # Calculate unifrac
+        unifrac_tse <- as.matrix(calculateUnifrac(tse))
+        unifrac_pseq <- as.matrix(UniFrac(pseq))
+        expect_equal(unifrac_tse, unifrac_pseq)
+        # Calculate unifrac
+        unifrac_tse <- as.matrix(calculateUnifrac(tse, weighted = TRUE, normalized = FALSE))
+        unifrac_pseq <- as.matrix(UniFrac(pseq, weighted = TRUE, normalized = FALSE))
+        expect_equal(unifrac_tse, unifrac_pseq)
+        
+        # Test with merged object with multiple trees
+        tse <- mergeSEs(GlobalPatterns, esophagus)
+        
+        # Convert data into phyloseq
+        pseq <- makePhyloseqFromTreeSE(tse)
+        # Calculate unifrac
+        unifrac_tse <- as.matrix(calculateUnifrac(tse))
+        unifrac_pseq <- as.matrix(UniFrac(pseq))
+        expect_equal(unifrac_tse, unifrac_pseq)
+        # Calculate unifrac
+        unifrac_tse <- as.matrix(calculateUnifrac(tse, weighted = TRUE, normalized = FALSE, 
+                                                  tree_name = "phylo.1"))
+        unifrac_pseq <- as.matrix(UniFrac(pseq, weighted = TRUE, normalized = FALSE))
+        expect_equal(unifrac_tse, unifrac_pseq)
+    }
 })
