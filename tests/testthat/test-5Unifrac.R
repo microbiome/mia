0a33b910ad4beb91416a721f5bd9ea4769ef4144
Tuomas Borman <tvborm@utu.fi>
2022-10-10 19:17:19 +0300

    up

--- tests/testthat/test-5Unifrac.R
+++ tests/testthat/test-5Unifrac.R
@@ -54,45 +54,43 @@ test_that("Unifrac beta diversity", {
     
     data("GlobalPatterns")
     tse <- GlobalPatterns
-    # Compare to phyloseq function if it is installed
-    if( require("phyloseq") ){
-        # Convert data into phyloseq
-        pseq <- makePhyloseqFromTreeSE(tse)
-        # Calculate unifrac
-        unifrac_tse <- as.matrix(calculateUnifrac(tse))
-        unifrac_pseq <- as.matrix(UniFrac(pseq))
-        expect_equal(unifrac_tse, unifrac_pseq)
-        # Calculate unifrac
-        unifrac_tse <- as.matrix(calculateUnifrac(tse, weighted = TRUE, normalized = FALSE))
-        unifrac_pseq <- as.matrix(UniFrac(pseq, weighted = TRUE, normalized = FALSE))
-        expect_equal(unifrac_tse, unifrac_pseq)
-        
-        # Test with merged object with multiple trees
-        tse <- mergeSEs(GlobalPatterns, esophagus, missing_values = 0)
-        
-        # Convert data into phyloseq
-        pseq <- makePhyloseqFromTreeSE(tse)
-        # Convert back to TreeSE (pseq has pruned tree)
-        tse <- makeTreeSEFromPhyloseq(pseq)
-        # Calculate unifrac
-        unifrac_tse <- as.matrix(calculateUnifrac(tse))
-        unifrac_pseq <- as.matrix(UniFrac(pseq))
-        expect_equal(unifrac_tse, unifrac_pseq)
-        # Calculate unifrac
-        unifrac_tse <- as.matrix(calculateUnifrac(tse, weighted = TRUE, normalized = FALSE, 
-                                                  tree_name = "phylo"))
-        unifrac_pseq <- as.matrix(UniFrac(pseq, weighted = TRUE, normalized = FALSE))
-        expect_equal(unifrac_tse, unifrac_pseq)
-        
-        # Test the funcction with agglomerated data
-        tse <- agglomerateByRank(tse, rank = "Phylum")
-        # Convert data into phyloseq
-        suppressWarnings( pseq <- makePhyloseqFromTreeSE(tse) )
-        # Convert back to TreeSE (pseq has pruned tree)
-        tse <- makeTreeSEFromPhyloseq(pseq)
-        # Calculate unifrac
-        unifrac_tse <- as.matrix(calculateUnifrac(tse, normalized = TRUE))
-        unifrac_pseq <- as.matrix(UniFrac(pseq, normalized = TRUE))
-        expect_equal(unifrac_tse, unifrac_pseq)
-    }
+    # Compare to phyloseq function
+    # Convert data into phyloseq
+    pseq <- makePhyloseqFromTreeSE(tse)
+    # Calculate unifrac
+    unifrac_tse <- as.matrix(calculateUnifrac(tse))
+    unifrac_pseq <- as.matrix(phyloseq::UniFrac(pseq))
+    expect_equal(unifrac_tse, unifrac_pseq)
+    # Calculate unifrac
+    unifrac_tse <- as.matrix(calculateUnifrac(tse, weighted = TRUE, normalized = FALSE))
+    unifrac_pseq <- as.matrix(phyloseq::UniFrac(pseq, weighted = TRUE, normalized = FALSE))
+    expect_equal(unifrac_tse, unifrac_pseq)
+    
+    # Test with merged object with multiple trees
+    tse <- mergeSEs(GlobalPatterns, esophagus, missing_values = 0)
+    
+    # Convert data into phyloseq
+    pseq <- makePhyloseqFromTreeSE(tse)
+    # Convert back to TreeSE (pseq has pruned tree)
+    tse <- makeTreeSEFromPhyloseq(pseq)
+    # Calculate unifrac
+    unifrac_tse <- as.matrix(calculateUnifrac(tse))
+    unifrac_pseq <- as.matrix(phyloseq::UniFrac(pseq))
+    expect_equal(unifrac_tse, unifrac_pseq)
+    # Calculate unifrac
+    unifrac_tse <- as.matrix(calculateUnifrac(tse, weighted = TRUE, normalized = FALSE, 
+                                              tree_name = "phylo"))
+    unifrac_pseq <- as.matrix(phyloseq::UniFrac(pseq, weighted = TRUE, normalized = FALSE))
+    expect_equal(unifrac_tse, unifrac_pseq)
+    
+    # Test the funcction with agglomerated data
+    tse <- agglomerateByRank(tse, rank = "Phylum")
+    # Convert data into phyloseq
+    suppressWarnings( pseq <- makePhyloseqFromTreeSE(tse) )
+    # Convert back to TreeSE (pseq has pruned tree)
+    tse <- makeTreeSEFromPhyloseq(pseq)
+    # Calculate unifrac
+    unifrac_tse <- as.matrix(calculateUnifrac(tse, normalized = TRUE))
+    unifrac_pseq <- as.matrix(phyloseq::UniFrac(pseq, normalized = TRUE))
+    expect_equal(unifrac_tse, unifrac_pseq)
 })
