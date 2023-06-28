0eb6548587812ee7ff92ab7c8aa5c18ba06c05f3
BananaCancer <basil.courbayre@ecole.ensicaen.fr>
2023-06-28 15:39:46 +0300

    Updated tests to expect warnings

--- tests/testthat/test-7dmn.R
+++ tests/testthat/test-7dmn.R
@@ -1,16 +1,16 @@
 context("DMN")
 test_that("DMN", {
     data(dmn_se)
-    actual <- calculateDMN(dmn_se)
+    actual <- expect_warning(calculateDMN(dmn_se))
     expect_true(is.list(actual))
     expect_s4_class(actual[[1L]],"DMN")
     # return a list of DMN objects
-    actual <- getDMN(dmn_se)
+    actual <- expect_warning(getDMN(dmn_se))
     expect_true(is.list(actual))
     expect_s4_class(actual[[1L]],"DMN")
     # return, which objects fits best
-    expect_equal(bestDMNFit(dmn_se, type = "laplace"), 4)
+    expect_equal(expect_warning(bestDMNFit(dmn_se, type = "laplace")), 4)
     # return the model, which fits best
-    actual <- getBestDMNFit(dmn_se, type = "laplace")
+    actual <- expect_warning(getBestDMNFit(dmn_se, type = "laplace"))
     expect_s4_class(actual,"DMN")
 })
