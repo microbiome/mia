7665c88f3306b502a472eedebd0c1e6083a0da07
Daenarys8 <rysdaena8@gmail.com>
2023-07-03 14:15:16 +0100

    Up
    Signed-off-by: Daenarys8 <rysdaena8@gmail.com>

--- inst/scripts/import.R
+++ inst/scripts/import.R
@@ -6,8 +6,8 @@ biom_file_path <- "PATH_TO_BIOM_FILE"
 sample_meta_file_path <- "PATH_TO_SAMPLE_METADATA_FILE"
 tree_file_path <- "PATH_TO_PHYLOGENETIC_TREE_FILE"
 
-# Load data from the biom file into a SummarizedExperiment container
-se <- loadFromBiom(biom_file_path, removeTaxaPrefixes = TRUE, rankFromPrefix = TRUE)
+# Load data from the biom file into a TreeSummarizedExperiment container
+tse <- loadFromBiom(biom_file_path, removeTaxaPrefixes = TRUE, rankFromPrefix = TRUE)
 
 # Read sample metadata from file and add column names if necessary
 sample_meta <-
@@ -23,11 +23,11 @@ colnames(sample_meta) <- c("patient_status", "cohort",
 
 # Add sample metadata to colData slot of the TSE object
 # Note that the data must be given in a DataFrame format (required for our purposes)
-colData(se) <- cbind(colData(se), DataFrame(sample_meta))
+colData(tse) <- cbind(colData(tse), DataFrame(sample_meta))
 
 # Read the phylogenetic tree from file and assign it to the rowTree slot of the TSE object
 tree <- ape::read.tree(tree_file_path)
-rowTree(se) <- tree
+rowTree(tse) <- tree
 
 # Save the final TSE object as an R data file
-saveRDS(se, file = "OUTPUT_FILE_NAME.rds")
+saveRDS(tse, file = "OUTPUT_FILE_NAME.rds")
