709cce98a3afb3ef9c3b3ab718a3b8aa5c22eb8e
Daenarys8 <rysdaena8@gmail.com>
2023-07-03 13:31:54 +0100

    Replace mode of removing taxa prefixes
    Signed-off-by: Daenarys8 <rysdaena8@gmail.com>

--- inst/scripts/import.R
+++ inst/scripts/import.R
@@ -7,32 +7,11 @@ sample_meta_file_path <- "PATH_TO_SAMPLE_METADATA_FILE"
 tree_file_path <- "PATH_TO_PHYLOGENETIC_TREE_FILE"
 
 # Load data from the biom file into a SummarizedExperiment container
-se <- loadFromBiom(biom_file_path)
-
-# Convert the SummarizedExperiment to a TreeSummarizedExperiment container
-tse <- as(se, "TreeSummarizedExperiment")
-
-# We notice that the rowData fields do not have descriptive names.
-# Hence, let us rename the columns in rowData
-names(rowData(tse)) <- c("Kingdom", "Phylum", "Class", "Order", 
-                         "Family", "Genus")
-
-
-# We also notice that the taxa names are of form "c__Bacteroidia" etc.
-# Goes through the whole DataFrame. Removes '.*[kpcofg]__' from strings, where [kpcofg] 
-# is any character from listed ones, and .* any character.
-rowdata_modified <- BiocParallel::bplapply(rowData(tse), 
-                                           FUN = stringr::str_remove, 
-                                           pattern = '.*[kpcofg]__')
-
-# Genus level has additional '\"', so let's delete that also
-rowdata_modified <- BiocParallel::bplapply(rowdata_modified, 
-                                           FUN = stringr::str_remove, 
-                                           pattern = '\"')
+se <- loadFromBiom(biom_file_path, removeTaxaPrefixes = TRUE, rankFromPrefix = TRUE)
 
 # rowdata_modified is a list, so convert this back to DataFrame format. 
 # and assign the cleaned data back to the TSE rowData
-rowData(tse) <- DataFrame(rowdata_modified)
+rowData(se) <- DataFrame(rowdata_modified)
 
 # Read sample metadata from file and add column names if necessary
 sample_meta <-
@@ -52,7 +31,7 @@ colData(tse) <- cbind(colData(tse), DataFrame(sample_meta))
 
 # Read the phylogenetic tree from file and assign it to the rowTree slot of the TSE object
 tree <- ape::read.tree(tree_file_path)
-rowTree(tse) <- tree
+rowTree(se) <- tree
 
 # Save the final TSE object as an R data file
-saveRDS(tse, file = "OUTPUT_FILE_NAME.rds")
+saveRDS(se, file = "OUTPUT_FILE_NAME.rds")
