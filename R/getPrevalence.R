c1a92d73b12e0532cc58a940dcc757426fec6630
Tuomas Borman <60338854+TuomasBorman@users.noreply.github.com>
2023-07-21 13:38:33 +0300

    Add signifance testing to RDA/CCA (#381)
    Co-authored-by: Giulio Benedetti <giuliobene2000@gmail.com>
    Co-authored-by: Leo Lahti <leo.lahti@iki.fi>

--- R/getPrevalence.R
+++ R/getPrevalence.R
@@ -31,8 +31,11 @@
 #'
 #' @param other_label A single \code{character} valued used as the label for the
 #'   summary of non-prevalent taxa. (default: \code{other_label = "Other"})
+#'   
+#' @param rank a single character defining a taxonomic rank. Must be a value of
+#'   \code{taxonomyRanks()} function.
 #'
-#' @param rank,... additional arguments
+#' @param ... additional arguments
 #' \itemize{
 #'   \item{If \code{!is.null(rank)} arguments are passed on to
 #'     \code{\link[=agglomerate-methods]{agglomerateByRank}}. See
