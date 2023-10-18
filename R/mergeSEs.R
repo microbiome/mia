4933280563995020bffb0c2ab618ec0acd4a044b
erawijantari <erawijantari@gmail.com>
2023-10-18 13:42:48 +0300

    fix note and warning BiocCheck

--- R/mergeSEs.R
+++ R/mergeSEs.R
@@ -369,7 +369,7 @@ setMethod("right_join", signature = c(x = "ANY"),
 
     # Loop through individual TreeSEs and add them to tse
     if( length(x) > 0 ){
-        for( i in 1:length(x) ){
+        for( i in seq_len(length(x)) ){
             # Give message if TRUE
             if( verbose ){
                 message("\r", i+1, "/", length(x)+1, appendLF = FALSE)
