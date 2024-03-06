535ec6f2862e38bc8415b05b4eee21a9432511db
thpralas <theotime.pralas@centrale-med.fr>
2024-03-06 16:09:47 +0200

    up

--- R/deprecate.R
+++ R/deprecate.R
@@ -3,47 +3,23 @@
 #' @param x A
 #'   \code{\link[SummarizedExperiment:SummarizedExperiment-class]{SummarizedExperiment}}
 #'   object.
-#'   
-#' @param clust.col A single character value indicating the name of the 
-#'   \code{rowData} (or \code{colData}) where the data will be stored.
-#'   
-#' @param exprs_values a single \code{character} value for specifying which
-#'   assay to use for calculation.
-#'   (Please use \code{assay.type} instead.)
-#'   
-#' @param transposed Logical scalar, is x transposed with cells in rows?
-#' 
-#' @param type the type of measure used for the goodness of fit. One of
-#'   \sQuote{laplace}, \sQuote{AIC} or \sQuote{BIC}.
-#'   
-#' @inheritParams bluster::clusterRows
-#' @inheritParams runDMN
-#' @inheritParams transformAssay
-#' 
-#' @param x a \code{\link{SummarizedExperiment}} object -
-#' 
-#' @param ... Additional parameters to use altExps for example
 #' 
 #' @name deprecate
 NULL
 
 #' @rdname deprecate
 setGeneric("cluster", signature = c("x"),
-            function(x, BLUSPARAM, assay.type = assay_name, 
-                    assay_name = "counts", MARGIN = "features", full = FALSE, 
-                    name = "clusters", clust.col = "clusters", ...)
+            function(x,...)
                 standardGeneric("cluster"))
 
 #' @rdname deprecate
 #' @export
 #' @importFrom bluster clusterRows
 setMethod("cluster", signature = c(x = "SummarizedExperiment"),
-            function(x, BLUSPARAM, assay.type = assay_name, 
-                    assay_name = "counts", MARGIN = "features", full = FALSE, 
-                    name = "clusters", clust.col = "clusters", ...){
+            function(x,...){
                 .Deprecated(msg = paste0("'cluster' is deprecated. ",
                                         "Use 'addCluster' instead."))
-                addCluster(x)
+                addCluster(x,...)
             }
 )
 
@@ -56,18 +32,17 @@ setGeneric("addTaxonomyTree",
 #' @rdname deprecate
 #' @export
 setMethod("addTaxonomyTree", signature = c(x = "SummarizedExperiment"),
-            function(x){
+            function(x,...){
                 .Deprecated(msg = paste0("'addTaxonomyTree' is deprecated.",
                                         "Use 'addHierarchyTree' instead."))
-                addHierarchyTree(x)
+                addHierarchyTree(x,...)
             }
 )
 
 #' @rdname deprecate
 #' @export
 setMethod("calculateDMN", signature = c(x = "SummarizedExperiment"),
-            function(x, assay.type = assay_name, assay_name = exprs_values, exprs_values = "counts", 
-                    transposed = FALSE, ...){
+            function(x,...){
                 .Deprecated(msg = paste0("'calculateDMN' is deprecated. ",
                                         "Use 'addCluster' with DMMParam parameter instead."))
                 mat <- assay(x, assay.type)
@@ -150,9 +125,9 @@ setGeneric("taxonomyTree",
 #' @rdname deprecate
 #' @export
 setMethod("taxonomyTree", signature = c(x = "SummarizedExperiment"),
-            function(x){
+            function(x,...){
                 .Deprecated(msg = paste0("'taxonomyTree' is deprecated. ",
                                         "Use 'getHierarchyTree' instead."))
-                getHierarchyTree(x)
+                getHierarchyTree(x,...)
             }
 )
