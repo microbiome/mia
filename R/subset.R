c48d1d84121a6bc64160bc2c8d7a22b5a03f67da
thpralas <theotime.pralas@centrale-med.fr>
2024-04-18 11:44:14 +0300

    move subset functions back to their file that will be deleted eventually

--- R/subset.R
+++ R/subset.R
@@ -1,3 +1,16 @@
+#' @export
+setGeneric("subsetSamples", signature = "x",
+           function(x, ...)
+               standardGeneric("subsetSamples"))
+#' @export
+setGeneric("subsetFeatures", signature = "x",
+           function(x, ...)
+               standardGeneric("subsetFeatures"))
+#' @export
+setGeneric("subsetTaxa", signature = "x",
+           function(x, ...)
+               standardGeneric("subsetTaxa"))
+
 .get_subset_args <- function(x, subset = NULL, select = NULL, ...){
     rows <- subset
     columns <- select
@@ -9,3 +22,32 @@
     }
     return(list(rows = rows, columns = columns))
 }
+
+#' @export
+setMethod("subsetSamples", signature = "SummarizedExperiment",
+          function(x, ...){
+              .Deprecated(msg = paste0("subsetSamples is deprecated. Please ",
+                                       "use '[]' for subsetting instead."))
+              subset_args <- .get_subset_args(x, ...)
+              x[subset_args$columns,subset_args$rows]
+          }
+)
+
+#' @export
+setMethod("subsetFeatures", signature = "SummarizedExperiment",
+          function(x, ...){
+              .Deprecated(msg = paste0("subsetFeatures is deprecated. Please",
+                                       " use '[]' for subsetting instead."))
+              subset_args <- .get_subset_args(x, ...)
+              x[subset_args$rows, subset_args$columns]
+          }
+)
+
+#' @export
+setMethod("subsetTaxa", signature = "SummarizedExperiment",
+          function(x, ...){
+              .Deprecated(msg = paste0("subsetFeatures is deprecated. Please",
+                                       " use '[]' for subsetting instead."))
+              subsetFeatures(x, ...)
+          }
+)
