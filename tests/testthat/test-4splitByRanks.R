ab020c6da0987eb3ffd67efb50906abc9ae06b57
TuomasBorman <tvborm@utu.fi>
2024-05-22 08:04:15 +0300

    up

--- tests/testthat/test-4splitByRanks.R
+++ tests/testthat/test-4splitByRanks.R
@@ -10,6 +10,13 @@ test_that("agglomerateByRanks", {
     altExp(x,"Kingdom")
     expect_equal(dim(altExp(x,"Kingdom")),c(2,26))
     expect_equal(dim(altExp(x,"Species")),c(944,26))
+    
+    # Chceck that agglomerateByRanks and splitByRanks results to same output
+    res1 <- altExps(x)
+    res2 <- splitByRanks(x)
+    expect_equal( names(res1), names(res2) )
+    expect_equal( res1[[1]], res2[[1]] )
+    expect_equal( res1[[3]], res2[[3]] )
 
     # unsplitByRanks
     x2 <- unsplitByRanks(x)
