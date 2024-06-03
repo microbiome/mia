f3a16697444e03ec1145f62f581db837cde5e063
thpralas <theotime.pralas@centrale-med.fr>
2024-06-03 15:11:39 +0300

    try to handle aggregated data in unifrac tests

--- tests/testthat/test-5Unifrac.R
+++ tests/testthat/test-5Unifrac.R
@@ -50,23 +50,32 @@ test_that("Unifrac beta diversity", {
     
     # Test with merged object with multiple trees
     tse <- mergeSEs(GlobalPatterns, esophagus, assay.type="counts", missing_values = 0)
-    
+    tse_replaced <- tse
+    links <- rowLinks(tse_replaced)
+    links[ links$whichTree != "phylo", ] <- NA
+    links <- links[ , "nodeLab" ]
+    rownames(tse_replaced) <- links
     # Calculate unifrac
     unifrac_mia <- as.matrix(calculateUnifrac(tse, weighted = FALSE))
-    unifrac_rbiom <- as.matrix(rbiom::unifrac(assay(tse), weighted = FALSE,
-                                              rowTree(tse)))
+    unifrac_rbiom <- as.matrix(rbiom::unifrac(assay(tse_replaced), weighted = FALSE,
+                                              rowTree(tse_replaced)))
     expect_equal(unifrac_mia, unifrac_rbiom)
     # Calculate unifrac
     unifrac_mia <- as.matrix(calculateUnifrac(tse, weighted = TRUE))
-    unifrac_rbiom <- as.matrix(rbiom::unifrac(assay(tse), weighted = TRUE,
-                                              rowTree(tse)))
+    unifrac_rbiom <- as.matrix(rbiom::unifrac(assay(tse_replaced), weighted = TRUE,
+                                              rowTree(tse_replaced)))
     expect_equal(unifrac_mia, unifrac_rbiom)
     
     # Test the function with agglomerated data
     tse <- agglomerateByRank(tse, rank = "Phylum")
+    tse_replaced <- tse
+    links <- rowLinks(tse_replaced)
+    links[ links$whichTree != "phylo", ] <- NA
+    links <- links[ , "nodeLab" ]
+    rownames(tse_replaced) <- links
     # Calculate unifrac
     unifrac_mia <- as.matrix(calculateUnifrac(tse, weighted = FALSE))
-    unifrac_rbiom <- as.matrix(rbiom::unifrac(assay(tse), weighted = FALSE,
-                                              rowTree(tse)))
+    unifrac_rbiom <- as.matrix(rbiom::unifrac(assay(tse_replaced), weighted = FALSE,
+                                              rowTree(tse_replaced)))
     expect_equal(unifrac_mia, unifrac_rbiom)
 })
