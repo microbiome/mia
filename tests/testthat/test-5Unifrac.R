596f1a9f00d121c377214be5c2dab530e16aaeef
TuomasBorman <tvborm@utu.fi>
2024-06-10 10:12:33 +0300

    up

--- tests/testthat/test-5Unifrac.R
+++ tests/testthat/test-5Unifrac.R
@@ -46,34 +46,33 @@ test_that("Unifrac beta diversity", {
                                               rowTree(tse)))
     expect_equal(unifrac_mia, unifrac_rbiom)
     
-    # Test with merged object with multiple trees
+    # Test with merged object with multiple trees. runUnifrac takes subset of
+    # data based on provided tree.
     tse <- mergeSEs(GlobalPatterns, esophagus, assay.type="counts", missing_values = 0)
+    tse_ref <- tse
+    tse_ref <- tse_ref[ rowLinks(tse_ref)[["whichTree"]] == "phylo", ]
     # Calculate unifrac
     unifrac_mia <- as.matrix(calculateUnifrac(tse, weighted = FALSE))
-    tse_replaced <- tse
-    nodeLab <- rowLinks(tse)[ , "nodeLab" ]
-    if( any( !nodeLab %in% rowTree(tse, "phylo")$tip.label ) ||
-        any( !rowTree(tse, "phylo")$tip.label %in% nodeLab) ){
-      rowTree(tse, "phylo") <- .prune_tree(rowTree(tse, "phylo"), nodeLab)
-      warning("Pruning tree...", call. = FALSE)
-    }
-    rownames(tse_replaced) <- rowLinks(tse)[,"nodeLab"]
-    unifrac_rbiom <- as.matrix(rbiom::unifrac(assay(tse_replaced), weighted = FALSE,
-                                              rowTree(tse_replaced)))
+    unifrac_rbiom <- as.matrix(rbiom::unifrac(assay(tse_ref), weighted = FALSE,
+                                              rowTree(tse_ref)))
     expect_equal(unifrac_mia, unifrac_rbiom)
     # Calculate unifrac
     unifrac_mia <- as.matrix(calculateUnifrac(tse, weighted = TRUE))
-    unifrac_rbiom <- as.matrix(rbiom::unifrac(assay(tse_replaced), weighted = TRUE,
-                                              rowTree(tse_replaced)))
+    unifrac_rbiom <- as.matrix(rbiom::unifrac(assay(tse_ref), weighted = TRUE,
+                                              rowTree(tse_ref)))
     expect_equal(unifrac_mia, unifrac_rbiom)
     
-    # Test the function with agglomerated data
+    # Test the function with agglomerated data. runUnifrac renames rownames
+    # based on tips and links to them. Then it also prunes the tree so that
+    # rows are in tips.
+    tse <- GlobalPatterns
     tse <- agglomerateByRank(tse, rank = "Phylum")
-    tse_replaced <- tse
-    rownames(tse_replaced) <- rowLinks(tse)[,"nodeLab"]
+    tse_ref <- tse
+    rownames(tse_ref) <- rowLinks(tse_ref)[["nodeLab"]]
+    rowTree(tse_ref) <- .prune_tree(rowTree(tse_ref), rowLinks(tse_ref)[["nodeLab"]])
     # Calculate unifrac
     unifrac_mia <- as.matrix(calculateUnifrac(tse, weighted = FALSE))
-    unifrac_rbiom <- as.matrix(rbiom::unifrac(assay(tse_replaced), weighted = FALSE,
-                                              rowTree(tse_replaced)))
+    unifrac_rbiom <- as.matrix(rbiom::unifrac(assay(tse_ref), weighted = FALSE,
+                                              rowTree(tse_ref)))
     expect_equal(unifrac_mia, unifrac_rbiom)
 })
