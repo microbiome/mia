68c7240f7b5a61417fccbb6d80bcded4aaa2a96f
thpralas <theotime.pralas@centrale-med.fr>
2024-07-24 16:10:16 +0300

    wrapper for nmf::NMF and feature loadings computation

---
+++ R/addNMF.R
@@ -0,0 +1,104 @@
+#' Non-negative Matrix Factorization
+#'
+#' These functions perform Non-negative Matrix Factorization on data stored in a 
+#'  \code{\link[TreeSummarizedExperiment:TreeSummarizedExperiment-class]{TreeSummarizedExperiment}}
+#'  object.
+#' 
+#' @param x a \code{\link[TreeSummarizedExperiment:TreeSummarizedExperiment-class]{TreeSummarizedExperiment}}
+#'  object.
+#'   
+#' @param k \code{Integer vector}. A number of latent vectors/topics. 
+#'  (Default: \code{2})
+#' 
+#' @param name \code{Character scalar}. The name to be used to store the result 
+#'  in the reducedDims of the output. (Default: \code{"NMF"})
+#'  
+#' @param assay.type \code{Character scalar}. Specifies which assay to use for 
+#'  NMF ordination. (Default: \code{"counts"})
+#' 
+#' @param ... optional arguments passed to \code{nmf::NMF}
+#' 
+#' @return 
+#' For \code{getNMF}, the ordination matrix with feature loadings matrix
+#'  as attribute \code{"loadings"}.
+#'  
+#' For \code{addNMF}, a \code{\link[TreeSummarizedExperiment:TreeSummarizedExperiment-class]{TreeSummarizedExperiment}}
+#'  object is returned containing the ordination matrix in reducedDims(..., name)
+#'  with feature loadings matrix as attribute \code{"loadings"}.
+#'  
+#' @details 
+#' The functions \code{getNMF} and \code{addNMF} internally use \code{nmf::NMF} 
+#'  compute the ordination matrix and 
+#'  feature loadings.
+#'  
+#' @name addNMF
+#' 
+#' @examples
+#' data(GlobalPatterns)
+#' tse <- GlobalPatterns
+#' 
+#' # Reduce the number of features 
+#' tse <- agglomerateByPrevalence(tse, rank="Phylum")
+#' 
+#' # Run NMF and add the result to reducedDim(tse, "NMF")
+#' tse <- addNMF(tse)
+#' 
+#' # Extract feature loadings
+#' loadings <- attr(reducedDim(tse, "NMF"), "loadings")
+#' head(loadings)
+#' 
+NULL
+
+#' @rdname addNMF
+#' @export
+setGeneric("getNMF", signature = c("x"),
+           function(x, ...)
+             standardGeneric("getNMF"))
+
+#' @rdname addNMF
+#' @export
+setGeneric("addNMF", signature = c("x"),
+           function(x, ...)
+             standardGeneric("addNMF"))
+
+#' @export
+#' @rdname addNMF
+setMethod("getNMF", "SummarizedExperiment",
+          function(x, k = 2, assay.type = "counts", ...){
+            .require_package("nmf")
+            # Input checks
+            if( !.is_integer(k) ){
+              stop("'k' must be an integer.", call. = FALSE)
+            }
+            .check_assay_present(assay.type, x)
+            ######################################
+            mat <- t(assay(x, assay.type))
+            # Calculate nmf model
+            nmf <- nmf(x, k)
+            # store scores
+            scores <- nmf@fit@W
+            # Add loadings as attribute of the scores matrix
+            attr(scores, "loadings") <- nmf@fit@H
+            # Add NMF model as attribute of the scores matrix
+            attr(scores, "model") <- nmf
+            # Return scores with loadings, metrics and model as attribute
+            return(scores)
+          }
+)
+
+#' @export
+#' @rdname addNMF
+setMethod("addNMF", "SummarizedExperiment",
+          function(x, k = 2, assay.type = "counts", name = "NMF", ...){
+            # Input checks
+            if( !.is_a_string(name) ){
+              stop("'name' must be a non-empty single character value.",
+                   call. = FALSE)
+            }
+            # Fit the model
+            nmf <- t(getNMF(x, k, assay.type, ...))
+            # Add scores matrix with loadings as attribute to reducedDims
+            x <- .add_values_to_reducedDims(x, name, values = scores)
+            return(x)
+          }
+)
