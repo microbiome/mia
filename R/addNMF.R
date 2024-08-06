613522fadf56f650a4264db25dd81f4b4a952c28
thpralas <theotime.pralas@centrale-med.fr>
2024-08-06 18:06:00 +0200

    eval metrics solution for NMF

--- R/addNMF.R
+++ R/addNMF.R
@@ -15,8 +15,14 @@
 #'  
 #' @param assay.type \code{Character scalar}. Specifies which assay to use for 
 #'  NMF ordination. (Default: \code{"counts"})
+#'  
+#' @param eval.metric \code{Character scalar}. Specifies evaluation metric that
+#' will be used to select the model with the best fit. Must be in
+#' \code{c("evar", "sparseness.basis", "sparseness.coef", "rss", "evar",
+#'  "silhouette.coef", "silhouette.basis", "cophenetic", "dispersion")}.
+#'  (Default: \code{"evar"})
 #' 
-#' @param ... optional arguments passed to \code{nmf::NMF}
+#' @param ... optional arguments passed to \code{nmf::NMF}.
 #' 
 #' @return 
 #' For \code{getNMF}, the ordination matrix with feature loadings matrix
@@ -24,16 +30,19 @@
 #'  
 #' For \code{addNMF}, a \code{\link[TreeSummarizedExperiment:TreeSummarizedExperiment-class]{TreeSummarizedExperiment}}
 #'  object is returned containing the ordination matrix in reducedDims(..., name)
-#'  with feature loadings matrix as attribute \code{"loadings"}.
+#'  with the following attributes: 
+#'  "loadings" which is a matrix containing the feature loadings
+#'  "NMF_output" which is the output of function \code{nmf::NMF} 
+#'  "best_fit" which is the result of the best fit if k is a vector of integers
 #'  
 #' @details 
 #' The functions \code{getNMF} and \code{addNMF} internally use \code{nmf::NMF} 
 #'  compute the ordination matrix and 
 #'  feature loadings.
 #'  
-#' All NMF scores for rank values from 2 to 10 are calculated and the rank value
-#'  with highest explained variance is selected. The scores for this rank value 
-#'  are returned with the NMF model and feature loadings as attributes.
+#' If k is a vector of integers, NMF output is calculated for all the rank 
+#'  values contained in k, and the best fit is selected based on 
+#'  \code{eval.metric} value.
 #'  
 #' @name addNMF
 #' 
@@ -45,11 +54,18 @@
 #' tse <- agglomerateByPrevalence(tse, rank = "Phylum")
 #' 
 #' # Run NMF and add the result to reducedDim(tse, "NMF")
-#' tse <- addNMF(tse)
+#' tse <- addNMF(tse, k = 2, name = "NMF")
 #' 
 #' # Extract feature loadings
-#' loadings <- attr(reducedDim(tse, "NMF"), "loadings")
-#' head(loadings)
+#' loadings_NMF <- attr(reducedDim(tse, "NMF"), "loadings")
+#' head(loadings_NMF)
+#' 
+#' # Estimate models with number of topics from 2 to 4
+#' tse <- addNMF(tse, k = c(2,3,4), name = "NMF_4")
+#' 
+#' # Extract feature loadings
+#' loadings_NMF_4 <- attr(reducedDim(tse, "NMF_4"), "loadings")
+#' head(loadings_NMF_4)
 #' 
 NULL
 
@@ -68,7 +84,7 @@ setGeneric("addNMF", signature = c("x"),
 #' @export
 #' @rdname addNMF
 setMethod("getNMF", "SummarizedExperiment",
-    function(x, assay.type = "counts", ...){
+    function(x, k = 2, assay.type = "counts", eval.metric = "evar", ...){
         .require_package("NMF")
         # Both NmF and DelayedArray have method seed(). When running
         # NMF::nmf() an error occurs due to wrong method. That is why NMF
@@ -79,18 +95,24 @@ setMethod("getNMF", "SummarizedExperiment",
         }
         library("NMF")
         .check_assay_present(assay.type, x)
-        mat <- t(assay(x, assay.type))
-        # Calculate nmf scores for different rank values
-        k = c(2, 3, 4, 5, 6, 7, 8, 9, 10)
-        nmf_rank <- NMF::nmf(mat, k)
-        max_i <- which.max(nmf_rank$measures$evar) + 1
-        # store scores for the rank with highest explained variance 
-        scores <- nmf_rank[["fit"]][[as.character(max_i)]]@fit@W
-        # Add loadings as attribute of the scores matrix
-        attr(scores, "loadings") <- 
-                              t(nmf_rank[["fit"]][[as.character(max_i)]]@fit@H)
-        # Add NMF output as attribute of the scores matrix
-        attr(scores, "output") <- nmf_rank
+        
+        mat <- t(assay(x))
+        res <- NMF::nmf(mat, k)
+        
+        if( is(res, "NMF.rank") ){
+          best_fit <- .get_best_nmf_fit(res, eval.metric, ...)
+        } else{
+          best_fit <- res
+        }
+        
+        scores <- best_fit@fit@W
+        loadings <- best_fit@fit@H
+        
+        attr(scores, "loadings") <- t(loadings)
+        attr(scores, "NMF_output") <- res
+        if( is(res, "NMF.rank") ){
+          attr(scores, "best_fit") <- best_fit
+        }
         # The NMF package is unloaded
         detach("package:NMF", unload = TRUE)
         # Return scores with loadings, metrics and model as attribute
@@ -101,7 +123,8 @@ setMethod("getNMF", "SummarizedExperiment",
 #' @export
 #' @rdname addNMF
 setMethod("addNMF", "SummarizedExperiment",
-    function(x, k = 2, assay.type = "counts", name = "NMF", ...){
+    function(x, k = 2, assay.type = "counts", eval.metric = "evar",
+              name = "NMF", ...){
         # Input checks
         if( !.is_a_string(name) ){
             stop("'name' must be a non-empty single character value.",
@@ -114,3 +137,29 @@ setMethod("addNMF", "SummarizedExperiment",
         return(x)
     }
 )
+
+################################ HELP FUNCTIONS ################################
+# This function is for evaluating a fit of NMF models
+.get_best_nmf_fit <- function(res, eval.metric){
+  maximize <- c(
+    "sparseness.basis" = TRUE,
+    "sparseness.coef" = TRUE,
+    "rss" = FALSE,
+    "evar" = TRUE,
+    "silhouette.coef" = TRUE,
+    "silhouette.basis" = TRUE,
+    "cophenetic" = TRUE,
+    "dispersion" = FALSE)
+  maximize <- maximize[ eval.metric ]
+  measures <- res[["measures"]]
+  values <- measures[[eval.metric]]
+  if( maximize ){
+    FUN <- which.max
+  } else{
+    FUN <- which.min
+  }
+  ind <- FUN(values)
+  #
+  model <- res[["fit"]][[ind]]
+  return(model)
+}
