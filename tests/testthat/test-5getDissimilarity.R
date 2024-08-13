ee1557e073032e78a89ad7f1486d19c9aaf56465
thpralas <theotime.pralas@centrale-med.fr>
2024-08-13 15:56:34 +0200

    up

--- tests/testthat/test-5getDissimilarity.R
+++ tests/testthat/test-5getDissimilarity.R
@@ -9,28 +9,10 @@ test_that("Dissimilarity calculation", {
   mat <- assay(tse, "counts")
   expect_equal(as.matrix(vegan::vegdist(t(mat), "euclidean")),
                as.matrix(getDissimilarity(tse, method = "euclidean")))
-  # Test rarefaction
-  ntop <- 5
-  tse_sub <- tse[head(rev(order(rowSds(assay(tse, "counts")))), ntop), ]
-  mat <- assay(tse_sub, "counts")
-  clr <- function (x) {
-    vegan::decostand(x, method="clr", pseudocount=1)
-  }
-  set.seed(123)
-  res1 <- vegan::avgdist(t(mat), distfun = vegdist, dmethod = "euclidean",
-                          sample = min(colSums2(mat)), iterations = 10, 
-                          transf = clr)
-  set.seed(123)
-  res2 <- getDissimilarity(tse_sub, method = "euclidean", niter = 10, 
-                           transf = clr)
   tse <- GlobalPatterns
   # Test if Unifrac dissimilarity works with external tree
   expect_equal(getDissimilarity(tse, method = "unifrac"),
                getDissimilarity(tse, tree = rowTree(tse), method = "unifrac"))
-  # Test if results from vegan::vegdist are equal to getDissimilarity results
-  mat <- assay(tse, "counts")
-  expect_equal(as.matrix(vegan::vegdist(t(mat), "euclidean")),
-               as.matrix(getDissimilarity(tse, method = "euclidean")))
   # Test rarefaction
   ntop <- 5
   tse_sub <- tse[head(rev(order(rowSds(assay(tse, "counts")))), ntop), ]
@@ -40,8 +22,8 @@ test_that("Dissimilarity calculation", {
   }
   set.seed(123)
   res1 <- vegan::avgdist(t(mat), distfun = vegdist, dmethod = "euclidean",
-                         sample = min(colSums2(mat)), iterations = 10, 
-                         transf = clr)
+                         sample = as.integer(min(colSums2(mat))), 
+                         iterations = 10, transf = clr)
   set.seed(123)
   res2 <- getDissimilarity(tse_sub, method = "euclidean", niter = 10, 
                            transf = clr)
