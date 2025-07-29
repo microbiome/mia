961b3e763b89aaa54e847a067173aca5aabdc242
Eetu Tammi <eotamm@utu.fi>
2025-07-29 11:15:00 +0300

    Update transformCounts.cpp

--- src/transformCounts.cpp
+++ src/transformCounts.cpp
@@ -7,27 +7,9 @@ S4 apply_transformation_difference(NumericMatrix mat) {
     int n_samples  = mat.ncol();
     int n_pairs    = n_features * (n_features - 1) / 2;
   
-    // Check if over 1000 features
-    if( n_features > 1000 ){
-        Rcpp::warning("The input matrix has over 1000 features, which may "
-                      "cause memory or performance issues. Consider subsetting "
-                      "or using fewer features.");
-    }
-  
-    // Generate default rownames if not present
-    CharacterVector original_rownames = rownames(mat);
-    bool has_valid_rownames = !Rf_isNull(original_rownames) && 
-         original_rownames.size() == n_features;
-  
-    if( !has_valid_rownames ){
-        Rcpp::warning("No rownames found in the matrix. "
-                      "Generated labels like Feature1 will be used.");
-    }
-  
     // Prepare output vectors
     std::vector<int> i_vec, j_vec;
     std::vector<double> x_vec;
-    CharacterVector rownames(n_pairs);
   
     int pair_idx = 0;
     for( int i = 0; i < n_features - 1; ++i ){
@@ -40,16 +22,6 @@ S4 apply_transformation_difference(NumericMatrix mat) {
                     x_vec.push_back(diff);
                 }
             }
-            // Rownames
-            std::string name_i = has_valid_rownames
-                ? as<std::string>(original_rownames[i])
-                : "Feature" + std::to_string(i + 1);
-          
-            std::string name_j = has_valid_rownames
-                ? as<std::string>(original_rownames[j])
-                : "Feature" + std::to_string(j + 1);
-          
-            rownames[pair_idx] = "diff_" + name_i + "-" + name_j;
             ++pair_idx;
         }
     }
@@ -60,7 +32,7 @@ S4 apply_transformation_difference(NumericMatrix mat) {
     tmat.slot("j") = IntegerVector(j_vec.begin(), j_vec.end());
     tmat.slot("x") = NumericVector(x_vec.begin(), x_vec.end());
     tmat.slot("Dim") = IntegerVector::create(n_pairs, n_samples);
-    tmat.slot("Dimnames") = List::create(rownames, colnames(mat));
+    tmat.slot("Dimnames") = List::create(R_NilValue, colnames(mat));
   
     // Convert dgTMatrix to dgCMatrix
     Function as("as");
@@ -71,52 +43,15 @@ S4 apply_transformation_difference(NumericMatrix mat) {
     return cmat;
 }
 
-
-
 // [[Rcpp::export(name = ".apply_transformation_division")]]
 S4 apply_transformation_division(NumericMatrix mat, double pseudocount = 1e-6) {
     int n_features = mat.nrow();
     int n_samples  = mat.ncol();
     int n_pairs    = n_features * (n_features - 1) / 2;
-  
-    // Check if over 1000 features
-    if( n_features > 1000 ){
-        Rcpp::warning("The input matrix has over 1000 features, which may "
-                      "cause memory or performance issues. Consider subsetting "
-                      "or using fewer features.");
-    }
-    
-    // Add pseudocount if needed
-    if( pseudocount > 0 ){
-        for( int i = 0; i < n_features; ++i ){
-            for( int j = 0; j < n_samples; ++j ){
-                if( mat(i, j) == 0.0 ){
-                    Rcpp::warning("Zero values detected in input matrix. "
-                                  "Pseudocount will be added.");
-                    i = n_features;
-                    break;
-                }
-            }
-        }
-        for( int i = 0; i < n_features; ++i )
-            for( int j = 0; j < n_samples; ++j )
-                mat(i, j) += pseudocount;
-    }
-    
-    // Generate default rownames if not present
-    CharacterVector original_rownames = rownames(mat);
-    bool has_valid_rownames = !Rf_isNull(original_rownames) && 
-        original_rownames.size() == n_features;
-    
-        if( !has_valid_rownames ){
-            Rcpp::warning("No rownames found in the matrix. "
-                          "Generated labels like Feature1 will be used.");
-        }
     
     // Prepare output vectors
     std::vector<int> i_vec, j_vec;
     std::vector<double> x_vec;
-    CharacterVector rownames(n_pairs);
   
     int pair_idx = 0;
     for( int i = 0; i < n_features - 1; ++i ){
@@ -132,16 +67,6 @@ S4 apply_transformation_division(NumericMatrix mat, double pseudocount = 1e-6) {
                     }
                 }
             }
-            // Rownames
-            std::string name_i = has_valid_rownames
-                ? as<std::string>(original_rownames[i])
-                : "Feature" + std::to_string(i + 1);
-          
-            std::string name_j = has_valid_rownames
-                ? as<std::string>(original_rownames[j])
-                : "Feature" + std::to_string(j + 1);
-          
-            rownames[pair_idx] = "div_" + name_i + "/" + name_j;
             ++pair_idx;
         }
     }
@@ -152,7 +77,7 @@ S4 apply_transformation_division(NumericMatrix mat, double pseudocount = 1e-6) {
     tmat.slot("j") = IntegerVector(j_vec.begin(), j_vec.end());
     tmat.slot("x") = NumericVector(x_vec.begin(), x_vec.end());
     tmat.slot("Dim") = IntegerVector::create(n_pairs, n_samples);
-    tmat.slot("Dimnames") = List::create(rownames, colnames(mat));
+    tmat.slot("Dimnames") = List::create(R_NilValue, colnames(mat));
   
     // Convert dgTMatrix to dgCMatrix
     Function as("as");
