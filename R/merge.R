74c5c98314d0b02e3a37457a2de55d77233126b7
Tuomas Borman <60338854+TuomasBorman@users.noreply.github.com>
2025-12-08 10:21:41 +0200

    Fixes issue 767 (#793)

--- R/merge.R
+++ R/merge.R
@@ -89,6 +89,11 @@
     # If user specified column name from row/colData, get the values
     if( .is_a_string(f) && f %in% colnames(rowData_FUN(x)) ){
         f <- rowData_FUN(x)[[ f ]]
+    } else if( .is_a_string(f) && !f %in% colnames(rowData_FUN(x)) &&
+                nrow_FUN(x) != 1L){
+        stop("Specified grouoing variable ('group' or 'rank' argument) must ",
+            "specify a column from ", switch(by, "row", "col"), "Data(x).",
+            call. = FALSE)
     }
     # Check that the group ID vector is specifying groups for each element
     f <- .norm_f(nrow_FUN(x), f, ...)
