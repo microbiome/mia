3c4fd9e80ecf788464a2726cf15e64cbd0aeede2
Tuomas Borman <60338854+TuomasBorman@users.noreply.github.com>
2026-09-22 11:27:29 +0300

    Convert MAE to TreeSE and back (#810)
    Co-authored-by: Muluh <127390183+0xMuluh@users.noreply.github.com>

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -435,3 +435,13 @@ setGeneric("addJointRPCA", signature = "x", function(x, ...)
 #' @export
 setGeneric("filterRPCAInput", signature = "x", function(x, ...)
     standardGeneric("filterRPCAInput"))
+
+           #' @rdname utilization_functions
+#' @export
+setGeneric("convertToMAE", signature = "x", function(x, ...)
+    standardGeneric("convertToMAE"))
+
+#' @rdname utilization_functions
+#' @export
+setGeneric("convertToTreeSE", signature = "x", function(x, ...)
+    standardGeneric("convertToTreeSE"))
